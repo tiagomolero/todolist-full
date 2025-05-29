d5c14c72758581017b9c8a7ccf1628a364399083
Tiago Molero <tiagomolero@gmail.com>
2025-05-29 15:37:29 -0300

    Criação do projeto API com o Spring Initializr

---
+++ api-todolist/src/test/java/br/molero/api_todolist/ApiTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.molero.api_todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
