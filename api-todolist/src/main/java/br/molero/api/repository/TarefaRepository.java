06bf30bb11a0da2cd7cf39c4a7bd146a245e9618
Tiago Molero <tiagomolero@gmail.com>
2025-05-31 17:44:10 -0300

    Criação da Interface Repository que será responsável pela conexão com o Banco de dados

---
+++ api-todolist/src/main/java/br/molero/api/repository/TarefaRepository.java
@@ -0,0 +1,9 @@
+package br.molero.api.repository;
+
+import br.molero.api.model.Tarefa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface TarefaRepository extends JpaRepository<Tarefa, UUID> {
+}
