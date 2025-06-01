5dbee31beda9ae8a6d4e703f5ca824c32982294d
Tiago Molero <tiagomolero@gmail.com>
2025-06-01 17:59:10 -0300

    Criação de tabela para apresentação das tarefas

--- todolist-app/src/app/tarefas/tarefas-module.ts
+++ todolist-app/src/app/tarefas/tarefas-module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { TarefasRoutingModule } from './tarefas-routing-module';
+import {MatTableModule} from '@angular/material/table';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    TarefasRoutingModule
+    TarefasRoutingModule,
+    MatTableModule
   ]
 })
 export class TarefasModule { }
--- todolist-app/src/app/tarefas/tarefas/tarefas.ts
+++ todolist-app/src/app/tarefas/tarefas/tarefas.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import {MatTableModule} from '@angular/material/table';
+
+export interface PeriodicElement {
+  titulo: string;
+  descricao: string;
+  status: number;
+  data: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {titulo: 'Tarefa 1', descricao: 'Descricao da Tarefa 1', status: 1, data: '23-05-2025'},
+  {titulo: 'Tarefa 2', descricao: 'Descricao da Tarefa 2', status: 2, data: '23-05-2025'},
+  {titulo: 'Tarefa 3', descricao: 'Descricao da Tarefa 3', status: 1, data: '23-05-2025'},
+  {titulo: 'Tarefa 4', descricao: 'Descricao da Tarefa 4', status: 3, data: '23-05-2025'},
+  {titulo: 'Tarefa 5', descricao: 'Descricao da Tarefa 5', status: 1, data: '23-05-2025'},
+  {titulo: 'Tarefa 6', descricao: 'Descricao da Tarefa 6', status: 3, data: '23-05-2025'},
+  {titulo: 'Tarefa 7', descricao: 'Descricao da Tarefa 7', status: 2, data: '23-05-2025'},
+];
 
 @Component({
   selector: 'app-tarefas',
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './tarefas.html',
   styleUrl: './tarefas.css'
 })
 export class Tarefas {
-
+  displayedColumns: string[] = ['titulo', 'descricao', 'status', 'data'];
+  dataSource = ELEMENT_DATA;
 }
