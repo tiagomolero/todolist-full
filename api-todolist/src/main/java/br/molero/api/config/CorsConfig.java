477e0af5e76ba02ade71bf553d676db6394639c0
Tiago Molero <tiagomolero@gmail.com>
2025-06-01 23:26:57 -0300

    Adicionado a configuração para que o app possa consumir a API

---
+++ api-todolist/src/main/java/br/molero/api/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package br.molero.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean // Indica que este método produz um bean gerenciado pelo Spring
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
